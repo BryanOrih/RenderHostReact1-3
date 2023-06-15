dfde9d42b28bcd68c1f91a278a0e8e2e76407167
Bryan Alvarez <balvarez@LABA.PSWAS.LOCAL>
2023-06-15 12:48:05 -0700

    finished 1

---
+++ host_1_react/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <p>
+          First website react practice on render
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
